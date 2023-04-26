78032ee6574991da67fcef8ffb84d37ed4073aa5
your name <binh.diepcucthit@hcmut.edu.vn>
2023-04-26 21:03:00 +0700

    delete all

--- test/driver.cpp
+++
@@ -1,5 +0,0 @@
-#define MAX_SIM 5000
-void set_random(Vtop *dut, vluint64_t sim_unit){
-    dut -> data_en_i = (sim_unit < 50);
-    dut -> input_text_i = 65+(int)(rand()*(69-65+1.0)/(1.0+RAND_MAX));
-}
